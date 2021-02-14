ba9f4c849fd4eac7825fabbd1db76fe0c887cf90
arunpusawant <arunpusawant@gmail.com>
2021-02-14 08:25:19 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,5 @@ apk add go
 go get github.com/ring04h/s5.go
 apk add nodejs
 apk add npm
-npm install wstunnel -g
-~/go/bin/s5.go -addr 0.0.0.0:3128 &
-wstunnel -s 0.0.0.0:8080
+npm install proxy -g
+proxy -p 8080
