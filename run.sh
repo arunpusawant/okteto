d2905443bdae439db33db12bdc74a85db49ee16b
arunpusawant <arunpusawant@gmail.com>
2021-02-16 04:43:46 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@ apk add git
 apk add go
 apk add curl
 wget "https://github.com/jpillora/cloud-torrent/releases/download/0.8.25/cloud-torrent_linux_amd64.gz"
+chmod +x cloud-torrent_linux_amd64.gz
 gzip -d cloud-torrent_linux_amd64.gz
 ./cloud-torrent_linux_amd64 --port 8080
